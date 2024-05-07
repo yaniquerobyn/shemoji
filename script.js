770c388441a79a9d7137c9bbe2ccce6b9add661f
Yanique <yaniquebarnett@hotmail.com>
2024-05-07 17:01:02 +0100

    updates

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const characterPreview = document.getElementById("character-preview");
     const coll = document.querySelectorAll(".collapsible");
-
+    const loader = document.getElementById('loader'); // Loader element
+ 
+ 
+ 
+ 
     // Add click event listeners to each heading
     coll.forEach(item => {
         item.addEventListener("click", function () {
@@ -22,93 +26,164 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
-
+ 
+ 
+     // Get all the checkboxes for the other features
+     var checkboxes = document.querySelectorAll('input[name="otherFeatures"]');
+ 
+ 
+     // Listen for changes on the checkboxes
+     checkboxes.forEach(function(checkbox) {
+         checkbox.addEventListener('change', function() {
+             updatePreview();
+         });
+     });
+    
     function updatePreview() {
+ 
+ 
         // Get the selected options
         const selectedBackground = document.querySelector('input[name="background"]:checked').value;
         const selectedSkin = document.querySelector('input[name="skin"]:checked').value;
         const selectedEyes = document.querySelector('input[name="eyes"]:checked').value;
         const selectedHairstyle = document.querySelector('input[name="hairstyle"]:checked').value;
-
+ 
+ 
         // Hide all hair color options
         const hairColorContainers = document.querySelectorAll('.color-options');
         hairColorContainers.forEach(container => {
             container.style.display = "none";
         });
-
+ 
+ 
         // Show hair color options for the selected hairstyle
         const selectedHairColorContainer = document.getElementById(selectedHairstyle + "-color-options");
         if (selectedHairColorContainer) {
             selectedHairColorContainer.style.display = "block";
         }
-
+   
         const selectedHairColor = document.querySelector('input[name="hair-color"]:checked').value;
         const selectedTops = document.querySelector('input[name="tops"]:checked').value;
         const selectedBottoms = document.querySelector('input[name="bottoms"]:checked').value;
         const selectedShoes = document.querySelector('input[name="shoes"]:checked').value;
         const selectedEarrings = document.querySelector('input[name="earrings"]:checked').value;
-        const selectedOtherFeatures = document.querySelector('input[name="otherFeatures"]:checked').value;
-
+ 
+ 
+       
         // Construct the path of the full-size image for each selected option
         const backgroundImagePath = 'fullImages/background/' + selectedBackground + '.png';
         const skinImagePath = 'fullImages/skin/' + selectedSkin + '.png';
         const eyesImagePath = 'fullImages/eyes/' + selectedEyes + '.png';
         const hairstyleImagePath = 'fullImages/hairstyle/' + selectedHairstyle + '/' + selectedHairColor + '.png';
-        const topsImagePath = 'fullImages/tops/' + selectedTops + '.png';
-        const bottomsImagePath = 'fullImages/bottoms/' + selectedBottoms + '.png';
         const shoesImagePath = 'fullImages/shoes/' + selectedShoes + '.png';
+        const bottomsImagePath = 'fullImages/bottoms/' + selectedBottoms + '.png';
+        const topsImagePath = 'fullImages/tops/' + selectedTops + '.png';
         const earringsImagePath = 'fullImages/earrings/' + selectedEarrings + '.png';
-        const otherFeaturesImagePath = 'fullImages/otherFeatures/' + selectedOtherFeatures + '.png';
-
-        // Construct the HTML for the stacked preview
-        const previewHTML = `
-            <img src="${backgroundImagePath}" alt="Background">
-            <img src="${skinImagePath}" alt="Skin Tone">
-            <img src="${eyesImagePath}" alt="Eye Color">
-            <img src="${otherFeaturesImagePath}" alt="Other Features">
-            <img src="${hairstyleImagePath}" alt="Hairstyle">
-            <img src="${shoesImagePath}" alt="Shoes">
-            <img src="${bottomsImagePath}" alt="Bottoms">
-            <img src="${topsImagePath}" alt="Tops">
-            <img src="${earringsImagePath}" alt="Earrings">
-        `;
-
-        // Update character preview with the stacked images
-        characterPreview.innerHTML = previewHTML;
+      //  const otherFeaturesImagePaths = Array.from(selectedOtherFeaturesNodes).map(node => 'fullImages/otherFeatures/' + node.value + '.png');       
+        const CameraImagePath = 'fullImages/camera.png';
+  // Handle multiple selections for otherFeatures
+  const selectedOtherFeaturesNodes = document.querySelectorAll('input[name="otherFeatures"]:checked');
+  let otherFeaturesImagePaths = [];
+  selectedOtherFeaturesNodes.forEach(node => {
+      const featureValue = node.value;
+      let imagePath;
+      if (featureValue === 'freckles') {
+          switch (selectedSkin) {
+              case 'fair':
+                  imagePath = 'fullImages/otherFeatures/frecklesFair.png';
+                  break;
+              case 'tan':
+                  imagePath = 'fullImages/otherFeatures/frecklesTan.png';
+                  break;
+              case 'deep':
+              case 'deeper':
+                  imagePath = 'fullImages/otherFeatures/frecklesDeep.png';
+                  break;
+              default:
+                  imagePath = 'fullImages/otherFeatures/' + featureValue + '.png';
+          }
+      } else {
+          imagePath = 'fullImages/otherFeatures/' + featureValue + '.png';
+      }
+      otherFeaturesImagePaths.push(imagePath);
+  });
+ 
+ 
+ 
+ 
+        // Construct an array of image paths
+        const imagePaths = [
+            backgroundImagePath,
+            skinImagePath,
+            eyesImagePath,
+            ...otherFeaturesImagePaths,
+            hairstyleImagePath,
+            shoesImagePath,
+            bottomsImagePath,
+            topsImagePath,
+            earringsImagePath,
+            CameraImagePath
+        ];
+ // Add the selected features to the character preview
+   // otherFeatures.forEach(function(feature) {
+   // var img = document.createElement('img');
+   // img.src = 'thumbnails/otherFeatures/' + feature + '.png';
+   // characterPreview.appendChild(img);
+ //});
+ 
+ 
+        const hiddenContainer = document.getElementById('hidden-container');
+ 
+ 
+        const imageLoadPromises = imagePaths.map(path => new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = () => {
+                hiddenContainer.appendChild(img);
+                resolve(path);
+            };
+            img.onerror = reject;
+            img.src = path;
+        }));
+        // Show the loader before starting the image loading process
+        loader.style.display = 'block';
+ 
+ 
+        Promise.all(imageLoadPromises).then((paths) => {
+            const previewHTML = paths.map(path => `<img src="${path}" alt="Character Image" style="position: absolute;">`).join('');
+            characterPreview.innerHTML = previewHTML;
+            hiddenContainer.innerHTML = ''; // Clear the hidden container
+       // Hide the loader when all images have loaded
+       loader.style.display = 'none';
+    }).catch(error => {
+        console.error("Failed to load images", error);
+ 
+ 
+        // Hide the loader if there was an error
+        loader.style.display = 'none';
+    });
     }
-
-    // Get all radio button options
-    const backgroundOptions = document.querySelectorAll('input[name="background"]');
-    const skinOptions = document.querySelectorAll('input[name="skin"]');
-    const eyesOptions = document.querySelectorAll('input[name="eyes"]');
-    const hairstyleOptions = document.querySelectorAll('input[name="hairstyle"]');
-    const hairColorOptions = document.querySelectorAll('input[name="hair-color"]');
-    const topsOptions = document.querySelectorAll('input[name="tops"]');
-    const bottomsOptions = document.querySelectorAll('input[name="bottoms"]');
-    const shoesOptions = document.querySelectorAll('input[name="shoes"]');
-    const earringsOptions = document.querySelectorAll('input[name="earrings"]');
-    const otherFeaturesOptions = document.querySelectorAll('input[name="otherFeatures"]');
-
-    // Add event listeners to all radio button options
-    backgroundOptions.forEach(option => option.addEventListener("change", updatePreview));
-    skinOptions.forEach(option => option.addEventListener("change", updatePreview));
-    eyesOptions.forEach(option => option.addEventListener("change", updatePreview));
-    hairstyleOptions.forEach(option => option.addEventListener("change", function () {
-        updatePreview();
-        // Trigger click event on the color section to ensure it's expanded
-        const selectedHairstyle = document.querySelector('input[name="hairstyle"]:checked').value;
-        const selectedHairColorSection = document.getElementById(selectedHairstyle + "-color-options");
-        if (selectedHairColorSection) {
-            selectedHairColorSection.previousElementSibling.click();
+ 
+ 
+    const options = document.querySelectorAll('input[type="radio"]');
+    options.forEach(option => option.addEventListener("change", updatePreview));
+ 
+ 
+    updatePreview(); // Initial preview update
+ 
+ 
+    function applyMobileConditions() {
+        if (window.innerWidth <= 600) {
+            const submitButton = document.getElementById('submit-button');
+            if (submitButton) {
+                submitButton.style.display = 'block';
+                submitButton.style.width = '80%';
+                submitButton.style.margin = '20px auto';
+            }
         }
-    }));
-    hairColorOptions.forEach(option => option.addEventListener("change", updatePreview));
-    topsOptions.forEach(option => option.addEventListener("change", updatePreview));
-    bottomsOptions.forEach(option => option.addEventListener("change", updatePreview));
-    shoesOptions.forEach(option => option.addEventListener("change", updatePreview));
-    earringsOptions.forEach(option => option.addEventListener("change", updatePreview));
-    otherFeaturesOptions.forEach(option => option.addEventListener("change", updatePreview));
-
-    // Initial preview update
-    updatePreview();
-});
+    }
+ 
+ 
+    window.addEventListener('resize', applyMobileConditions);
+    applyMobileConditions(); // Apply mobile conditions initially
+ });
+ 
